09229cfa718b079b6f4be02f52a27da350375a18
qinxuewu <870439570@qq.com>
2019-03-19 08:40:49 +0800

    AQS源码学习

--- src/com/example/juc/MyAQSLock.java
+++ src/com/example/juc/MyAQSLock.java
@@ -1,5 +1,4 @@
 package com.example.juc;
-
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.AbstractQueuedSynchronizer;
 import java.util.concurrent.locks.Condition;
@@ -12,8 +11,6 @@ import java.util.concurrent.locks.Lock;
  * @create 19/3/18下午11:44
  * @since 1.0.0
  */
-
-
 public class MyAQSLock implements Lock {
     private  final  MySync sync=new MySync();
 
@@ -21,17 +18,75 @@ public class MyAQSLock implements Lock {
      * 构建一个内部帮助器 集成AQS
      */
     private  static  class MySync extends AbstractQueuedSynchronizer{
+        //状态为0时获取锁，
 
+        /***
+         * 一个线程进来时，如果状态为0，就更改state变量，返回true表示拿到锁
+         *
+         * 当state大于0说明当前锁已经被持有，直接返回false,如果重复进来，就累加state,返回true
+         * @param arg
+         * @return
+         */
+        @Override
+        protected boolean tryAcquire(int arg) {
+            //获取同步状态状态的成员变量的值
+            int state=getState();
+            Thread cru=Thread.currentThread();
+            if(state==0){
+                //CAS方式更新state，保证原子性，期望值，更新的值
+                if( compareAndSetState(0,arg)){
+                    //设置成功
+                    //设置当前线程
+                    setExclusiveOwnerThread(Thread.currentThread());
+                    return  true;
+                }
+            }else if(Thread.currentThread()==getExclusiveOwnerThread()){
+                    //如果还是当前线程进来，累加state,返回true  可重入
+                    setState(state+1);
+                    return  true;
+            }
+            return false;
+        }
+
+        /**
+         * 释放同步状态
+         * @param arg
+         * @return
+         */
+        @Override
+        protected boolean tryRelease(int arg) {
+            boolean flag=false;
+            //判断释放操作是否是当前线程，
+            if(Thread.currentThread()==getExclusiveOwnerThread()){
+
+                    //获取同步状态成员变量，如果大于0 才释放
+                    int state=getState();
+                    if(getState()==0){
+                        //当前线程置为null
+                        setExclusiveOwnerThread(null);
+                        flag=true;
+                    }
+                    setState(arg);
+
+            }else{
+                //不是当线程抛出异常
+                throw  new RuntimeException();
+            }
+            return flag;
+        }
+        Condition newCondition(){
+            return  new ConditionObject();
+        }
     }
 
     @Override
     public void lock() {
-
+            sync.acquire(1);
     }
 
     @Override
     public void lockInterruptibly() throws InterruptedException {
-
+        sync.acquireInterruptibly(1);
     }
 
     /**
@@ -40,12 +95,12 @@ public class MyAQSLock implements Lock {
      */
     @Override
     public boolean tryLock() {
-        return false;
+        return sync.tryAcquire(1);
     }
 
     @Override
     public boolean tryLock(long time, TimeUnit unit) throws InterruptedException {
-        return false;
+        return sync.tryAcquireNanos(1,unit.toNanos(time));
     }
 
     /**
@@ -53,11 +108,11 @@ public class MyAQSLock implements Lock {
      */
     @Override
     public void unlock() {
-
+        sync.tryRelease(1);
     }
 
     @Override
     public Condition newCondition() {
-        return null;
+        return sync.newCondition();
     }
 }
---
+++ src/com/example/juc/MyAQSLockTest.java
@@ -0,0 +1,80 @@
+package com.example.juc;
+
+/**
+ * 〈一句话功能简述〉<br>
+ * 〈〉
+ *
+ * @author qinxuewu
+ * @create 19/3/19上午8:18
+ * @since 1.0.0
+ */
+
+
+public class MyAQSLockTest {
+    MyAQSLock lock=new MyAQSLock();
+    private    int i=0;
+    public  int  next() {
+        try {
+            lock.lock();
+            try {
+                Thread.sleep(300);
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return i++;
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
+        return 0;
+    }
+
+    public void test1(){
+        System.out.println("test1");
+        test2();
+    }
+    public  void  test2(){
+        System.out.println("test2");
+    }
+
+    public static void main(String[] args){
+        MyAQSLockTest test=new MyAQSLockTest();
+//         Thread thread = new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                while (true) {
+//
+//                    System.out.println(Thread.currentThread().getName() + "-" + test.next());
+//
+//                }
+//
+//            }
+//        });
+//        thread.start();
+//
+//        Thread thread2 = new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                while (true) {
+//
+//                    System.out.println(Thread.currentThread().getName() + "-" + test.next());
+//
+//                }
+//
+//            }
+//        });
+//        thread2.start();
+
+        //可重复锁演示
+        Thread thread3 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                test.test1();
+
+            }
+        });
+        thread3.start();
+    }
+}
