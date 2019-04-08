235ec16b455c4824ff8d6084341ec2101db78107
qinxuewu <870439570@qq.com>
2019-04-08 11:28:56 +0800

    leedcode每日一练

---
+++ src/com/example/LettCode131.java
@@ -0,0 +1,32 @@
+package com.example;
+
+import java.util.List;
+
+/**
+ * 分割回文串
+ *
+ * 给定一个字符串 s，将 s 分割成一些子串，使每个子串都是回文串。返回 s 所有可能的分割方案。
+ *
+     输入: "aab"
+     输出:
+     [
+       ["aa","b"],
+       ["a","a","b"]
+     ]
+ *
+ * @author qinxuewu
+ * @version 1.00
+ * @time  8/4/2019 上午 10:23
+ * @email 870439570@qq.com
+ */
+public class LettCode131 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public List<List<String>> partition(String s) {
+
+        return  null;
+    }
+}
