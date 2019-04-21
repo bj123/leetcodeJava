7fc4926e362687324383255f9b24c49850e2f2ae
qinxuewu <870439570@qq.com>
2019-04-21 10:00:56 +0800

    leetcode每日一练

--- src/com/example/LettCode21.java
+++ src/com/example/LettCode21.java
@@ -27,23 +27,6 @@ public class LettCode21 {
 		ListNode(int x) { 
 			val = x; 
 		}
-
-		public int getVal() {
-			return val;
-		}
-
-		public void setVal(int val) {
-			this.val = val;
-		}
-
-		public ListNode getNext() {
-			return next;
-		}
-
-		public void setNext(ListNode next) {
-			this.next = next;
-		}
-		
 		
 	}
 	
